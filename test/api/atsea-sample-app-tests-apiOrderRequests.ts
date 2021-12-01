ec589863969b115465dcd43459d62bf34d991687
marianabetancur18 <mbetancurf@unal.edu.com>
2021-12-01 17:24:06 -0500

    Order request test

--- test/api/atsea-sample-app-tests-apiOrderRequests.ts
+++ test/api/atsea-sample-app-tests-apiOrderRequests.ts
@@ -1,8 +1,8 @@
 // Imported all dependencies needed
-import { get, post} from 'superagent';
+import { get, post, del} from 'superagent';
 import { StatusCodes } from 'http-status-codes';
 import { expect } from 'chai';
-import { customerRegistration, customerDelete } from '../TestCostumerData';
+
 
 // Host for all requests
 const host = `http://localhost:8080`;
@@ -14,17 +14,47 @@ const month = (today.getMonth() + 1).toString().padStart(2, '0');
 const currentDate = `${today.getFullYear()}-${month}-${today.getDate()}`;
 
 
+let customerIf;
+
+let register = {
+    customerId: 0,
+    name: 'Luisa martinez',
+    address:'NISWANDEE SMALLSYS INC 795 E',
+    email: 'Lumar@gmail.com',
+    phone: '123 456 7809',
+    username: '1234567',
+    password: '1234567',
+    enabled: 'true',
+    role: 'USER'
+  };
+
 // Order informartion 
 let order = {
     orderId: 0,
     orderDate: currentDate,
-    customerId: customerRegistration().customerId,
+    customerId:customerIf,
     productsOrdered: { 1: 1, 2: 2, 3: 3 }
   };
+
 
   describe('Atsea shop API Endpoint Order Request', () => {
     let orderId;
     let response;
+// Since some test need the information of an user (order request for example) this script creates temporarily an user
+    describe('Create an temporily customer', () => {
+        before(async () => {
+          response = await post(`${host}/api/customer/`)
+            .set('User-Agent', 'agent')
+            .set('Content-Type', 'application/json')
+            .send(register);
+          customerIf = response.body.customerIf;
+        });
+        it('Customer created successfully', () => {
+          expect(response.status).to.equal(StatusCodes.CREATED);
+        });
+      });
+      register.customerId = customerIf;
+
 // Create an order
     describe('Creating an order', () => {
       before(async () => {
@@ -52,5 +82,14 @@ let order = {
         expect(response.body.customerId).to.equal(order.customerId);
       });
     });
-    customerDelete();
+    describe('Delate temporarily customer', () => {
+        before(async () => {
+          response = await del(`${host}/api/customer/`)
+          .set('User-Agent', 'agent')
+        });
+        it('temporarily customer successfully delated ', () => {
+          expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+        });
+      });
+
   });
